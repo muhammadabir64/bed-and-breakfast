98ed217c84dd66c4d5aeb75bad9781d10e7942f8
Muhammad ABir <muhammadabir99@gmail.com>
2023-09-24 23:56:30 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,5 @@ from src import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-	app.run(debug=True, port=8000) # debug=True only for development purpose
-
-# remove comments when upload into live server
-'''
 if __name__=="main":
 	app.run()
-'''
